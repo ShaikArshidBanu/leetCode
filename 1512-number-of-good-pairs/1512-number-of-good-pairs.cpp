5d30c6f56686c5252056eea6ff8d4961ccdec816
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-01-29 18:55:04 +0530

    Time: 0 ms (100.00%), Space: 9 MB (7.06%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         int ans=0;
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]==nums[j]) ans=ans+1;
-            }
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            ans+=mp[nums[i]];
+            mp[nums[i]]++;
         }
         return ans;
     }
