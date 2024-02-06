300cd74e20d85225d74f01f5f1c74965caf9cc31
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-02-06 23:25:18 +0530

    Time: 13 ms (13.49%), Space: 18 MB (12.34%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-        map<int,string>mp;
-        string ans;
+        string ans=s;
         for(int i=0;i<indices.size();i++){
-            mp[indices[i]]=s[i];
+            ans[indices[i]]=s[i];
         }
-       for(auto &str:mp){
-           ans+=str.second;
-       }
         return ans;
     }
 };
