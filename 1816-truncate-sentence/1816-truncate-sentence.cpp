ab9e524d4f90b7c351c532de8dc691a96ac99d0b
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-02-07 23:27:42 +0530

    Time: 4 ms (30.63%), Space: 7.6 MB (38.99%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        int cnt=0;
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ') {
+               cnt++;
+        }
+            if(cnt<k){
+                ans+=s[i];
+            }
+        }
+        return ans;
+    }
+};
