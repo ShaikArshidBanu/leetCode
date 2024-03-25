404a6afc373571ea94d1d843947bd123838756bc
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-03-25 19:13:17 +0530

    Time: 9 ms (69.57%), Space: 13.6 MB (47.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             int current=nums[i];
             int required=target-current;
-            if(mp.find(required)!= mp.end()) return {i,mp[required]};
+            if(mp.find(required)!=mp.end()) return {i,mp[required]};
             mp[current]=i;
         }
         return {-1};
