dcfe2ee980231b1316d52a2bf3762c4d79007721
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-03-26 23:13:32 +0530

    Time: 29 ms (5.75%), Space: 41.8 MB (5.06%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,16 +4,15 @@ public:
         int n=nums.size();
         vector<int>left(n,1);
         vector<int>right(n,1);
+        vector<int> ans;
         for(int i=1;i<n;i++){
             left[i]=left[i-1]*nums[i-1];
         }
-        
         for(int i=n-2;i>=0;i--){
             right[i]=right[i+1]*nums[i+1];
         }
-        vector<int>ans;
         for(int i=0;i<n;i++){
-            ans.push_back(right[i]*left[i]);
+            ans.push_back(left[i]*right[i]);
         }
         return ans;
     }
