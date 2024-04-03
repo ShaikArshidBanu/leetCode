894da47e726a2792a0c82b158d0d1749391cb301
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-04-03 18:36:14 +0530

    Time: 13 ms (26.04%), Space: 8.3 MB (96.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,6 +3,10 @@ public:
     bool isAnagram(string s, string t) {
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        return s==t;
+        if(s.size()!=t.size()) return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]) return false;
+        }
+        return true;
     }
 };
