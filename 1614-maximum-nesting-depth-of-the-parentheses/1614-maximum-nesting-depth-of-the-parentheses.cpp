4f2fdd0dc5f60ff9353c027412e8a994f87d4e8a
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-04-04 22:48:57 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (22.85%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count=0,max_count=0;
+        for(char c:s){
+            if(c=='('){
+                count++;
+                if(max_count<count){
+                    max_count=count;
+                }
+            }
+            else if(c==')') count--;
+        }
+        return max_count;
+    }
+};
