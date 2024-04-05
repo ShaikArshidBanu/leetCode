209ae18d3d2abac428e7b4f70a0b77e245d6e8b5
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-04-05 21:05:08 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (89.71%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans;
+        ans+=s[0];
+        for(int i=1;i<s.size();i++){
+            if(!ans.empty() and isupper(s[i]) and islower(ans.back()) and s[i]==toupper(ans.back()))
+                ans.pop_back();
+            else if(!ans.empty() and islower(s[i]) and isupper(ans.back()) and                                s[i]==tolower(ans.back()))
+                ans.pop_back();
+            else ans+=s[i];
+            
+        }
+        return ans;
+    }
+};
