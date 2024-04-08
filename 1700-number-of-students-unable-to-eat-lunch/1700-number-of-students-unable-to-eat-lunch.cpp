ec6dbd72dc8def5890c28567f5969587dd104f86
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-04-08 22:27:25 +0530

    Time: 5 ms (31.29%), Space: 11.3 MB (5.56%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -3,7 +3,7 @@ public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
         unordered_map<int,int>mp;
         for(auto stu:students) mp[stu]++;
-        int ans=sandwiches.size();
+        int ans=students.size();
         for(auto san:sandwiches){
             if(mp[san]>0){
                 ans--;
