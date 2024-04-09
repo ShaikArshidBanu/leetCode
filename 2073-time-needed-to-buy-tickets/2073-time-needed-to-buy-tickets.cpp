19acc9292193f2c447c6ba8210b44c1986e678a1
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-04-09 22:55:40 +0530

    Time: 0 ms (100.00%), Space: 9.2 MB (91.31%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -3,7 +3,6 @@ public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int ans=0;
         while(tickets[k]!=0){
-            if(tickets[k]==0) break;
             for(int i=0;i<tickets.size();i++){ 
                 if(tickets[i]>0){
                     tickets[i]=tickets[i]-1;
