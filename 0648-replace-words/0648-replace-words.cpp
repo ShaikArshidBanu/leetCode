83d15bad931f22e90cf6548c7d8ea1315897da84
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-06-07 22:42:23 +0530

    Time: 515 ms (24.30%), Space: 344.2 MB (17.55%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        string s;
+        stringstream ss(sentence);
+        vector<string>v;
+        while(getline(ss,s,' ')){
+            v.push_back(s);
+        }
+        for(int i=0;i<dictionary.size();i++){
+            string element=dictionary[i];
+        for(int j=0;j<v.size();j++){
+            if(v[j].substr(0,element.size())==element) v[j]=element;
+        }
+        
+    }
+        string ans="";
+        for(int i=0;i<v.size()-1;i++){
+            ans+=v[i]+" ";
+        }
+        ans+=v[v.size()-1];
+        return ans;
+        }
+};
