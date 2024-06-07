c48c2f92a6d48b90b5561ac4a454edf3fe1e899a
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-06-07 22:44:59 +0530

    Time: 478 ms (25.46%), Space: 344.2 MB (17.55%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -4,16 +4,18 @@ public:
         string s;
         stringstream ss(sentence);
         vector<string>v;
+        
         while(getline(ss,s,' ')){
             v.push_back(s);
         }
+        
         for(int i=0;i<dictionary.size();i++){
             string element=dictionary[i];
         for(int j=0;j<v.size();j++){
             if(v[j].substr(0,element.size())==element) v[j]=element;
-        }
-        
+        } 
     }
+        
         string ans="";
         for(int i=0;i<v.size()-1;i++){
             ans+=v[i]+" ";
@@ -21,4 +23,5 @@ public:
         ans+=v[v.size()-1];
         return ans;
         }
+    
 };
