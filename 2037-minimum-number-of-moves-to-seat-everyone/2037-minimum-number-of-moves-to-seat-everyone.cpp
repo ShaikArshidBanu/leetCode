8969ba8f23168ed34f84362566eda681313a06db
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-06-13 23:01:09 +0530

    Time: 7 ms (60.18%), Space: 21.1 MB (17.35%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -3,10 +3,10 @@ public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
-        int sum=0;
+        int ans=0;
         for(int i=0;i<seats.size();i++){
-            sum+=abs(seats[i]-students[i]);
+            ans+=abs(seats[i]-students[i]);
         }
-        return sum;
+        return ans;
     }
 };
