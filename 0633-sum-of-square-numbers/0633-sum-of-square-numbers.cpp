e15be7eff84478f7d1bcbb092480dd3f7faac3bc
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-06-17 22:49:46 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (61.35%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long a=0;
+        long long b= static_cast<long long>(sqrt(c));
+        while(a<=b){
+            long long sumOfSquares= a*a + b*b;
+            if(sumOfSquares==c) return true;
+            else if (sumOfSquares<c) ++a;
+            else --b;
+        }
+        return false;
+    }
+};
