6a5a79154e9b0118d18a7c3642f1fea4f935a8c1
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-07-22 23:02:25 +0530

    Time: 29 ms (60.95%), Space: 27.8 MB (27.02%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-       vector<string>ans;
-        priority_queue<pair<int,string>>pq;
+        map<int,string>mp;
         for(int i=0;i<names.size();i++){
-            pq.push({heights[i],names[i]});
+            mp[heights[i]]=names[i];
         }
-        while(!pq.empty()){
-            string temp=pq.top().second;
-            ans.push_back(temp);
-            pq.pop();
+    
+        vector<string>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
         }
+        reverse(ans.begin(),ans.end());
+        
         return ans;
     }
 };
