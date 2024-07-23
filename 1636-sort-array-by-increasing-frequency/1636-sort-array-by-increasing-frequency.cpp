830b1ae460063917dea5646d97579df8d7f2032a
Shaik Arshid Banu <112767165+ShaikArshidBanu@users.noreply.github.com>
2024-07-23 23:27:23 +0530

    Time: 8 ms (36.47%), Space: 15 MB (25.59%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -5,11 +5,10 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        
-       auto compare=[&](int a,int b){
-           if(mp[a]==mp[b]) return a>b;
-           return mp[a]<mp[b];
-       };
+        auto compare=[&](int a,int b){
+            if(mp[a]==mp[b]) return a>b;
+            return mp[a]<mp[b];
+        };
         sort(nums.begin(),nums.end(),compare);
         return nums;
     }
